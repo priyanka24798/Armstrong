67b8e1cc5a812d7bbdafee4bb14c460cdeadbb9e
priyanka24798 <kotupriyanka@msitprogram.net>
2020-07-12 20:49:26 +0530

    Jul 12  20:49:26

--- solution.py
+++ solution.py
@@ -1,7 +1,18 @@
 # Write a program determine whether the given number is Armstrong number or not.
 # The program should return true or false
 
-def checkArmstrong(num):
+def checkArmstrong():
+        n = int(input())
+        sum = 0
+        while(n > 0):
+                m = n % 10
+                sum = m **2
+                n = n//10
+        if(sum == n):
+                print("Armstrong Number")
+        else:
+                print("Not an armstrong number")
+
         # Your code goes here
 
-       return True
+
